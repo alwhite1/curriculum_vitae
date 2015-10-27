787a4b0eaa4f3a3d9d17154bcc7fb796f4e48e49
alwhite1 <alwhite1@rambler.ru>
2015-10-27 17:57:13 +0200

    Add test for model UserEducation

--- apps/users/factories.py
+++ apps/users/factories.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import factory
-from apps.users.models import UserInfo
+from .models import UserInfo
+from .models import UserEducation
 
 
 class UserInfoFactory(factory.Factory):
@@ -33,3 +34,28 @@ class CyrillicUserInfoFactory(factory.Factory):
     email = u"new@example.com"
     skype = u"new"
     phone_number = u"+38063142314"
+
+
+class UserEducationFactory(factory.Factory):
+
+    class Meta:
+        model = UserEducation
+
+    user_id = "1"
+    date_start = "2001-02-02"
+    date_end = "2005-02-02"
+    institution = "VNTU"
+    speciality = "master"
+
+
+class CyrillicUserEducationFactory(factory.Factory):
+
+    class Meta:
+        model = UserEducation
+
+    user_id = "1"
+    date_start = "2001-02-02"
+    date_end = "2005-02-02"
+    institution = "ВНТУ"
+    speciality = "Специалист"
+
--- apps/users/tests/test_models.py
+++ apps/users/tests/test_models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
-from apps.users.factories import UserInfoFactory, CyrillicUserInfoFactory
+from apps.users.factories import *
 from apps.users.models import UserInfo
 
 
@@ -16,6 +16,23 @@ class UserInfoModelTest(TestCase):
 
     def test_cyrillic_support(self):
         CyrillicUserInfoFactory.create().save()
-        UserInfo.objects.last().name
         self.assertEqual(UserInfo.objects.count(), 1)
         self.assertEqual(UserInfo.objects.last().name, u"Джон")
+
+
+class UserEducationModelTest(TestCase):
+
+    def test_can_create_new_object(self):
+        UserInfoFactory.create().save()
+        UserEducationFactory.create().save()
+        self.assertEqual(UserEducation.objects.count(), 1)
+
+    def test_can_delete_object(self):
+        UserEducation.objects.all().delete()
+        self.assertEqual(UserInfo.objects.count(), 0)
+
+    def test_cyrillic_support(self):
+        CyrillicUserInfoFactory.create().save()
+        CyrillicUserEducationFactory.create().save()
+        self.assertEqual(UserEducation.objects.count(), 1)
+        self.assertEqual(UserEducation.objects.last().institution, u"ВНТУ")

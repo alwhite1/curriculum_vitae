15335cdd093fa19a48a18f318403d6ce9a48696f
alwhite1 <alwhite1@rambler.ru>
2015-10-28 12:57:09 +0200

    add test for view api

--- apps/users/tests/test_views.py
+++ apps/users/tests/test_views.py
@@ -1,16 +1,30 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.test.client import Client
-from apps.users.views import main
+from apps.users.views import main, api
 
 
 class MainPageTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
 
     def test_root_url_resolves_to_main_page(self):
         found = resolve('/')
         self.assertEqual(found.func, main)
 
     def test_correct_main_page_template(self):
-        client = Client()
-        response = client.get('/')
+        response = self.client.get('/')
         self.assertTemplateUsed(response, 'main.html')
+
+    def test_api_url_resolves_to_api_page(self):
+        found = resolve('/api/')
+        self.assertEqual(found.func, api)
+
+    def test_api_ajax_request(self):
+        response = self.client.get('/api/', HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEqual(response.status_code, 200)
+
+    def test_api_not_ajax_request(self):
+        response = self.client.get('/api/', HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEqual(response.status_code, 404)
